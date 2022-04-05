b7a31b0495a1f9ed3e4820c035d455a2a5f0d561
My Name <cheshev0302@mail.ru>
2022-04-05 15:19:11 +0300

    planning list

--- satellite.py
+++ satellite.py
@@ -1,17 +1,13 @@
 import datetime
 import time
+import logging
+import skyfield.positionlib as poslib
 from datetime import timedelta
-
-import skyfield.positionlib
 from skyfield.api import load, Topos, EarthSatellite, wgs84, utc
 
 TLE_FILE = "https://celestrak.com/NORAD/elements/active.txt"
 
 satellites_name = []
-t_events_list = []
-temp_list_of_times = []
-list_of_times = []
-planning_list = []
 
 satellites_name.append("NOAA 18")
 satellites_name.append("NOAA 19")
@@ -19,13 +15,7 @@ satellites_name.append("NOAA 20")
 
 SATELLITES_COUNT = len(satellites_name)
 
-satellites = load.tle(TLE_FILE)
-
-_sats_by_name = {sat.name: sat for sat in satellites.values()}
-satellites_param = []
-for i in satellites_name:
-    satellites_param.append(_sats_by_name[i])
-
+logging.basicConfig(filename="log.log", level=logging.INFO)
 
 #############################################
 
@@ -54,70 +44,100 @@ class planningTime:
         self.distanse = distance
         self.index = index
 
-
 #############################################
 
 
-bluffton = wgs84.latlon(+60.006379, +30.379293)
-ts = load.timescale()
-t0 = ts.utc(2022, 4, 3, 5)
-t1 = ts.utc(2022, 4, 7, 23)
+while True:
 
-for i, val in enumerate(satellites_param):
-    t_i, events_i = val.find_events(bluffton, t0, t1, altitude_degrees=30.0)
-    t_events_list.append(tEvents(t_i, events_i))
+    satellites = load.tle(TLE_FILE)
+    _sats_by_name = {sat.name: sat for sat in satellites.values()}
+    satellites_param = []
 
-ti_rise = ts.now()
-ti_culm = ts.now()
-temp_alt, temp_az, temp_distance = skyfield.positionlib.Angle, skyfield.positionlib.Angle, skyfield.positionlib.Distance
-culm_degrees_flag = False
-step = 0
+    for i in satellites_name:
+        satellites_param.append(_sats_by_name[i])
 
-for i, val in enumerate(t_events_list):
-    for ti, event in zip(val.t, val.events):
-        if step == 0:
-            ti_rise = ti
-        elif step == 1:
-            ti_culm = ti
-            difference = satellites_param[i] - bluffton
-            topocentric = difference.at(ti_culm)
-            temp_alt, temp_az, temp_distance = topocentric.altaz()
-            if (temp_alt.degrees < 50):
-                culm_degrees_flag = True
-        elif culm_degrees_flag == False:
-            temp_list_of_times.append(setCulmRise(ti_rise, ti_culm, ti, temp_alt, temp_az, temp_distance, i))
-        if step == 2: culm_degrees_flag = False
-        step = 0 if step == 2 else step + 1
+    bluffton = wgs84.latlon(+60.006379, +30.379293)
+    ts = load.timescale()
+    t0 = ts.now()
+    t1 = t0 + timedelta(hours=20)
+    t_events_list = []
 
-temp_list_of_times.sort(key=lambda it: it.rise.tt)
+    for i, val in enumerate(satellites_param):
+        t_i, events_i = val.find_events(bluffton, t0, t1, altitude_degrees=30.0)
+        t_events_list.append(tEvents(t_i, events_i))
 
-for i, val in enumerate(temp_list_of_times):
-    if i == 0:
-        list_of_times.append(val)
-    else:
-        last_elem = list_of_times[len(list_of_times) - 1]
-        if (last_elem.rise.tt > val.rise.tt and last_elem.rise.tt < val.set.tt or
-            last_elem.set.tt > val.rise.tt and last_elem.set.tt < val.set.tt):
-            if (last_elem.alt.degrees < val.alt.degrees):
-                list_of_times.remove(last_elem)
+    temp_list_of_times = []
+    ti_rise = ts.now()
+    ti_culm = ts.now()
+    temp_alt, temp_az, temp_distance = poslib.Angle, poslib.Angle, poslib.Distance
+    culm_degrees_flag = False
+    step = 0
+
+    for i, val in enumerate(t_events_list):
+        for ti, event in zip(val.t, val.events):
+            if step == 0:
+                ti_rise = ti
+            elif step == 1:
+                ti_culm = ti
+                difference = satellites_param[i] - bluffton
+                topocentric = difference.at(ti_culm)
+                temp_alt, temp_az, temp_distance = topocentric.altaz()
+                if (temp_alt.degrees < 50):
+                    culm_degrees_flag = True
+            elif culm_degrees_flag == False:
+                temp_list_of_times.append(setCulmRise(ti_rise, ti_culm, ti, temp_alt, temp_az, temp_distance, i))
+            if step == 2: culm_degrees_flag = False
+            step = 0 if step == 2 else step + 1
+
+    temp_list_of_times.sort(key=lambda it: it.rise.tt)
+    list_of_times = []
+
+    for i, val in enumerate(temp_list_of_times):
+        if i == 0:
+            list_of_times.append(val)
+        else:
+            last_elem = list_of_times[len(list_of_times) - 1]
+            if (last_elem.rise.tt > val.rise.tt and last_elem.rise.tt < val.set.tt or
+                    last_elem.set.tt > val.rise.tt and last_elem.set.tt < val.set.tt):
+                if (last_elem.alt.degrees < val.alt.degrees):
+                    list_of_times.remove(last_elem)
+                    list_of_times.append(val)
+            else:
                 list_of_times.append(val)
-        else: list_of_times.append(val)
 
-for i in list_of_times:
-    print(satellites_name[i.index], "->",
-          "rise:", i.rise.utc_strftime('%Y %b %d %H:%M:%S'),
-          "; culmination:", i.culm.utc_strftime('%Y %b %d %H:%M:%S'),
-          "; set:", i.set.utc_strftime('%Y %b %d %H:%M:%S'))
-    curr = i.rise
-    while curr.tt <= i.set.tt:
-        curr += timedelta(seconds = 1)
-        difference = satellites_param[i.index] - bluffton
-        topocentric = difference.at(curr)
-        temp_alt, temp_az, temp_distance = topocentric.altaz()
-        planning_list.append(planningTime(curr, temp_alt, temp_az, temp_distance, i.index))
+    planning_list = []
 
-print()
+    for i in list_of_times:
+        # print(satellites_name[i.index], "-> rise:",
+        #       i.rise.utc_strftime('%Y %b %d %H:%M:%S'),
+        #       "; culmination:", i.culm.utc_strftime('%Y %b %d %H:%M:%S'),
+        #       "; set:", i.set.utc_strftime('%Y %b %d %H:%M:%S'))
+        logging.info(satellites_name[i.index] + " -> rise: " + i.rise.utc_strftime('%Y %m %d %H:%M:%S')
+                  + " ; culmination: " + i.culm.utc_strftime('%Y %m %d %H:%M:%S') + " ; set: " +
+                  i.set.utc_strftime('%Y %m %d %H:%M:%S'))
 
-for i in planning_list:
-    print(satellites_name[i.index], "::", i.time.utc_strftime('%Y %b %d %H:%M:%S'),
-          ";alt =", f"{i.alt.degrees:.{6}f}", "; az =", f"{i.az.degrees:.{6}f}")
+        curr = i.rise
+        while curr.tt <= i.set.tt:
+            curr += timedelta(seconds=1)
+            difference = satellites_param[i.index] - bluffton
+            topocentric = difference.at(curr)
+            temp_alt, temp_az, temp_distance = topocentric.altaz()
+            planning_list.append(planningTime(curr, temp_alt, temp_az, temp_distance, i.index))
+
+    # for i in planning_list:
+    #     print(satellites_name[i.index], "::", i.time.utc_strftime('%Y %b %d %H:%M:%S'),
+    #           ";alt =", f"{i.alt.degrees:.{6}f}", "; az =", f"{i.az.degrees:.{6}f}")
+
+    for val in planning_list:
+        curr_time = ts.now()
+        time_to_sleep = val.time.utc_datetime() - curr_time.utc_datetime()
+        logging.info("curr time : " + curr_time.utc_strftime('%Y %m %d %H:%M:%S') +
+                  " | sleep time : " + str(time_to_sleep.seconds) + " seconds or " +
+                  str(time_to_sleep.microseconds) + " microseconds")
+        # print("curr time :", curr_time.utc_strftime('%Y %b %d %H:%M:%S'),
+        #       "| sleep time :", time_to_sleep.seconds, "seconds or", time_to_sleep.microseconds, "microseconds")
+        time.sleep(time_to_sleep.microseconds)
+        #######
+        logging.info("time after sleep : " + val.time.utc_strftime('%Y %m %d %H:%M:%S'))
+        # print("time after sleep :", val.time.utc_strftime('%Y %b %d %H:%M:%S'), "\n------------------------")
+        #######
